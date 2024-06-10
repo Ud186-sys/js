af0f1b884270b32b72b818f55e13d3afd15916ae
Ud186-sys <81140614+Ud186-sys@users.noreply.github.com>
2024-06-10 17:22:06 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 14453
+let accountEmail = "test@google.com"
+var accountPassword = "1235"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail= "hc@hc.com"
+accountPassword="12112312"
+accountCity = "Banglore"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
